83e30284fefd7d9584fa1f083853765850778ae1
Adam Nemecek <adamnemecek@gmail.com>
2019-12-05 18:13:09 -0800

    stuff

--- src/view.rs
+++ src/view.rs
@@ -60,223 +60,181 @@ impl ViewRef {
     // weak open var delegate: MTKViewDelegate?
 
     pub fn device(&self) -> Option<DeviceRef> {
-        //impls: unsafe { msg_send![self, device] }
-        unimplemented!()
+        unsafe { msg_send![self, device] }
     }
 
     pub fn set_device(&self, new_value: Option<DeviceRef>) {
-        //impls: unsafe { msg_send![self, setDevice: new_value] }
-        unimplemented!()
+        unsafe { msg_send![self, setDevice: new_value] }
     }
 
     //get only
     pub fn current_drawable(&self) -> Option<CAMetalDrawable> {
-        //impls: unsafe { msg_send![self, currentDrawable] }
-        unimplemented!()
+        unsafe { msg_send![self, currentDrawable] }
     }
 
     pub fn framebuffer_only(&self) -> bool {
-        //impls: unsafe { msg_send![self, framebufferOnly] }
-        unimplemented!()
+        unsafe { msg_send![self, framebufferOnly] }
     }
 
     pub fn set_framebuffer_only(&self, new_value: bool) {
-        //impls: unsafe { msg_send![self, setFramebufferOnly: new_value] }
-        unimplemented!()
+        unsafe { msg_send![self, setFramebufferOnly: new_value] }
     }
 
     //@available(OSX 10.15, *)
     pub fn depth_stencil_attachment_texture_usage(&self) -> MTLTextureUsage {
-        //impls: unsafe { msg_send![self, depthStencilAttachmentTextureUsage] }
-        unimplemented!()
+        unsafe { msg_send![self, depthStencilAttachmentTextureUsage] }
     }
 
     pub fn set_depth_stencil_attachment_texture_usage(&self, new_value: MTLTextureUsage) {
-        //impls: unsafe { msg_send![self, setDepthStencilAttachmentTextureUsage: new_value] }
-        unimplemented!()
+        unsafe { msg_send![self, setDepthStencilAttachmentTextureUsage: new_value] }
     }
 
     //@available(OSX 10.15, *)
     pub fn multisample_color_attachment_texture_usage(&self) -> MTLTextureUsage {
-        //impls: unsafe { msg_send![self, multisampleColorAttachmentTextureUsage] }
-        unimplemented!()
+        unsafe { msg_send![self, multisampleColorAttachmentTextureUsage] }
     }
 
     pub fn set_multisample_color_attachment_texture_usage(&self, new_value: MTLTextureUsage) {
-        //impls: unsafe { msg_send![self, setMultisampleColorAttachmentTextureUsage: new_value] }
-        unimplemented!()
+        unsafe { msg_send![self, setMultisampleColorAttachmentTextureUsage: new_value] }
     }
 
     pub fn presents_with_transaction(&self) -> bool {
-        //impls: unsafe { msg_send![self, presentsWithTransaction] }
-        unimplemented!()
+        unsafe { msg_send![self, presentsWithTransaction] }
     }
 
     pub fn set_presents_with_transaction(&self, new_value: bool) {
-        //impls: unsafe { msg_send![self, setPresentsWithTransaction: new_value] }
-        unimplemented!()
+        unsafe { msg_send![self, setPresentsWithTransaction: new_value] }
     }
 
     pub fn color_pixel_format(&self) -> MTLPixelFormat {
-        //impls: unsafe { msg_send![self, colorPixelFormat] }
-        unimplemented!()
+        unsafe { msg_send![self, colorPixelFormat] }
     }
 
     pub fn set_color_pixel_format(&self, new_value: MTLPixelFormat) {
-        //impls: unsafe { msg_send![self, setColorPixelFormat: new_value] }
-        unimplemented!()
+        unsafe { msg_send![self, setColorPixelFormat: new_value] }
     }
 
     pub fn depth_stencil_pixel_format(&self) -> MTLPixelFormat {
-        //impls: unsafe { msg_send![self, depthStencilPixelFormat] }
-        unimplemented!()
+        unsafe { msg_send![self, depthStencilPixelFormat] }
     }
 
     pub fn set_depth_stencil_pixel_format(&self, new_value: MTLPixelFormat) {
-        //impls: unsafe { msg_send![self, setDepthStencilPixelFormat: new_value] }
-        unimplemented!()
+        unsafe { msg_send![self, setDepthStencilPixelFormat: new_value] }
     }
 
     pub fn sample_count(&self) -> Int {
-        //impls: unsafe { msg_send![self, sampleCount] }
-        unimplemented!()
+        unsafe { msg_send![self, sampleCount] }
     }
 
     pub fn set_sample_count(&self, new_value: Int) {
-        //impls: unsafe { msg_send![self, setSampleCount: new_value] }
-        unimplemented!()
+        unsafe { msg_send![self, setSampleCount: new_value] }
     }
 
     pub fn clear_color(&self) -> MTLClearColor {
-        //impls: unsafe { msg_send![self, clearColor] }
-        unimplemented!()
+        unsafe { msg_send![self, clearColor] }
     }
 
     pub fn set_clear_color(&self, new_value: MTLClearColor) {
-        //impls: unsafe { msg_send![self, setClearColor: new_value] }
-        unimplemented!()
+        unsafe { msg_send![self, setClearColor: new_value] }
     }
 
     pub fn clear_depth(&self) -> Double {
-        //impls: unsafe { msg_send![self, clearDepth] }
-        unimplemented!()
+        unsafe { msg_send![self, clearDepth] }
     }
 
     pub fn set_clear_depth(&self, new_value: Double) {
-        //impls: unsafe { msg_send![self, setClearDepth: new_value] }
-        unimplemented!()
+        unsafe { msg_send![self, setClearDepth: new_value] }
     }
 
     pub fn clear_stencil(&self) -> UInt32 {
-        //impls: unsafe { msg_send![self, clearStencil] }
-        unimplemented!()
+        unsafe { msg_send![self, clearStencil] }
     }
 
     pub fn set_clear_stencil(&self, new_value: UInt32) {
-        //impls: unsafe { msg_send![self, setClearStencil: new_value] }
-        unimplemented!()
+        unsafe { msg_send![self, setClearStencil: new_value] }
     }
 
     //get only
     pub fn depth_stencil_texture(&self) -> Option<MTLTexture> {
-        //impls: unsafe { msg_send![self, depthStencilTexture] }
-        unimplemented!()
+        unsafe { msg_send![self, depthStencilTexture] }
     }
 
     //get only
     pub fn multisample_color_texture(&self) -> Option<MTLTexture> {
-        //impls: unsafe { msg_send![self, multisampleColorTexture] }
-        unimplemented!()
+        unsafe { msg_send![self, multisampleColorTexture] }
     }
 
     pub fn release_drawables(&self) {
-        //impls: unsafe { msg_send![self, releaseDrawables] }
-        unimplemented!()
+        unsafe { msg_send![self, releaseDrawables] }
     }
 
     //get only
     pub fn current_render_pass_descriptor(&self) -> Option<MTLRenderPassDescriptor> {
-        //impls: unsafe { msg_send![self, currentRenderPassDescriptor] }
-        unimplemented!()
+        unsafe { msg_send![self, currentRenderPassDescriptor] }
     }
 
     pub fn preferred_frames_per_second(&self) -> Int {
-        //impls: unsafe { msg_send![self, preferredFramesPerSecond] }
-        unimplemented!()
+        unsafe { msg_send![self, preferredFramesPerSecond] }
     }
 
     pub fn set_preferred_frames_per_second(&self, new_value: Int) {
-        //impls: unsafe { msg_send![self, setPreferredFramesPerSecond: new_value] }
-        unimplemented!()
+        unsafe { msg_send![self, setPreferredFramesPerSecond: new_value] }
     }
 
     pub fn enable_setneeds_display(&self) -> bool {
-        //impls: unsafe { msg_send![self, enableSetneedsDisplay] }
-        unimplemented!()
+        unsafe { msg_send![self, enableSetneedsDisplay] }
     }
 
     pub fn set_enable_setneeds_display(&self, new_value: bool) {
-        //impls: unsafe { msg_send![self, setEnableEetneedsDisplay: new_value] }
-        unimplemented!()
+        unsafe { msg_send![self, setEnableEetneedsDisplay: new_value] }
     }
 
     pub fn autoresize_drawable(&self) -> bool {
-        //impls: unsafe { msg_send![self, autoresizeDrawable] }
-        unimplemented!()
+        unsafe { msg_send![self, autoresizeDrawable] }
     }
 
     pub fn set_autoresize_drawable(&self, new_value: bool) {
-        //impls: unsafe { msg_send![self, setAutoresizeDrawable: new_value] }
-        unimplemented!()
+        unsafe { msg_send![self, setAutoresizeDrawable: new_value] }
     }
 
     pub fn drawable_size(&self) -> CGSize {
-        //impls: unsafe { msg_send![self, drawableSize] }
-        unimplemented!()
+        unsafe { msg_send![self, drawableSize] }
     }
 
-    pub fn set_drawable_size(&self, size: CGSize) {
-        //impls: unsafe { msg_send![self, setDrawableSize: new_value] }
-        unimplemented!()
+    pub fn set_drawable_size(&self, new_value: CGSize) {
+        unsafe { msg_send![self, setDrawableSize: new_value] }
     }
 
     //get only
     //@available(OSX 10.15, *)
     pub fn preferred_drawable_size(&self) -> CGSize {
-        //impls: unsafe { msg_send![self, preferredDrawableSize] }
-        unimplemented!()
+        unsafe { msg_send![self, preferredDrawableSize] }
      }
 
     //get only
     //@available(OSX 10.15, *)
     pub fn preferred_device(&self) -> Option<MTLDevice> {
-        //impls: unsafe { msg_send![self, preferredDevice] }
-        unimplemented!()
+        unsafe { msg_send![self, preferredDevice] }
     }
 
     pub fn is_paused(&self) -> bool {
-        //impls: unsafe { msg_send![self, isPaused] }
-        unimplemented!()
+        unsafe { msg_send![self, isPaused] }
     }
 
     pub fn set_is_paused(&self, new_value: bool) {
-        //impls: unsafe { msg_send![self, setIsPaused: new_value] }
-        unimplemented!()
+        unsafe { msg_send![self, setIsPaused: new_value] }
     }
 
     //@available(OSX 10.12, *)
     pub fn colorspace(&self) -> Option<CGColorSpace> {
-        //impls: unsafe { msg_send![self, colorspace] }
-        unimplemented!()
+        unsafe { msg_send![self, colorspace] }
     }
 
     pub fn set_colorspace(&self, new_value: Option<CGColorSpace>) {
-        //impls: unsafe { msg_send![self, setColorspace: new_value] }
-        unimplemented!()
+        unsafe { msg_send![self, setColorspace: new_value] }
     }
 
     pub fn draw(&self) {
-        //impls: unsafe { msg_send![self, draw] }
-        unimplemented!()
+        unsafe { msg_send![self, draw] }
     }
 }
